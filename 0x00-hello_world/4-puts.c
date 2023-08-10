2812a75409408d1c90e2e7f07ff6f0bde2cded5a
0219kj <jk327950@gmail.com>
2023-08-10 12:23:54 -0700

    101-quote

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 
 /**
- *using puts() fxn in the main function
+ * testing usage of puts() function
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	/* return 0 for a successful program execution */
 	return (0);
 }

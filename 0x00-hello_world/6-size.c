3a6fbac47a9201d9e9ed38266ea975197889913c
0219kj <jk327950@gmail.com>
2023-08-10 11:24:20 -0700

    ammendment

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,11 @@
 */
 int main(void)
 {
-	printf("Size of int: %zu \n",sizeof(int));
-	printf("size of char: %zu \n", sizeof(char));
-	printf("size of float: %zu \n", sizeof(float));
-	printf("size of  double: %zu \n",sizeof(double));
+	printf("Size of char: %zu \n",sizeof(char));
+	printf("size of int: %zu \n", sizeof(int));
+	printf("size of long int: %zu \n", sizeof(long int));
+	printf("size of  long long int: %zu \n",sizeof(long long int));
+	printf("size of float: %zu \n",sizeof(float));
 
 	return (0);
 }
